6de3f305fe925b07fc4110468de0da18d52f3460
Tomas Zagorskis <tomzagos@gmail.com>
2022-10-19 11:19:08 +0300

    first launch meme generator app

---
+++ src/components/Meme.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import memesData from '../memesData';
+
+export default function Meme() {
+	const [memeImage, setMemeImage] = useState('');
+
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomIndex = Math.floor(Math.random() * memesArray.length);
+		setMemeImage(memesArray[randomIndex].url);
+	}
+	return (
+		<main>
+			<div className='form'>
+				<input type='text' placeholder='Top text' className='form--input' />
+				<input type='text' placeholder='Bottom text' className='form--input' />
+				<button onClick={getMemeImage} className='form--button'>
+					Get a new meme image 🖼
+				</button>
+			</div>
+			<img src={memeImage} alt='img' className='' />
+		</main>
+	);
+}
